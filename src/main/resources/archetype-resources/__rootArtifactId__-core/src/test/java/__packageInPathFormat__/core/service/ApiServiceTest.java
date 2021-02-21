b5aeaeba43a8dfe5969093e3cb91f73a6c30dc11
Aaron Prades <apradesdev@gmail.com>
2021-02-22 00:42:15 +0100

    Refactoring

--- src/main/resources/archetype-resources/__rootArtifactId__-core/src/test/java/__packageInPathFormat__/core/service/ApiServiceTest.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-core/src/test/java/__packageInPathFormat__/core/service/ApiServiceTest.java
@@ -3,10 +3,10 @@ package ${package}.core.service;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 @RunWith(MockitoJUnitRunner.class)
