6f00a68bfbcdd193bc921e262295ecd6ad290a38
Aaron Prades <apradesdev@gmail.com>
2021-02-24 02:01:10 +0100

    Added JPA module and datasource configuration

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-jpa/src/main/java/__packageInPathFormat__/jpa/mapper/ApiMapper.java
@@ -0,0 +1,12 @@
+package ${package}.jpa.mapper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiMapper {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ApiMapper.class);
+
+}
